c05a54c105af41e25a9f7312df49cacf7342635d
Nicholas Owen Weber <noweber@gmail.com>
2020-12-06 16:41:10 -0800

    load_data()

--- questions/questions.py
+++ questions/questions.py
@@ -1,4 +1,5 @@
 import nltk
+import os
 import sys
 
 FILE_MATCHES = 1
@@ -48,7 +49,29 @@ def load_files(directory):
     Given a directory name, return a dictionary mapping the filename of each
     `.txt` file inside that directory to the file's contents as a string.
     """
-    raise NotImplementedError
+
+    # TODO: use os.listdir and functions similar to traffic .. listdir() and path.join from project 5 are essential
+    # TODO: you should explicitly only look for .txt files within the directory
+
+    files = dict()
+
+    # Find each file within the given directory:
+    for filename in os.listdir(directory):
+        print("filename: ", filename)
+        if '.txt' in filename:
+            print("text file found!")
+            with open(os.path.join(directory, filename), encoding="utf8") as file:
+                # file_text = [
+                #     word.lower() for word in
+                #     nltk.word_tokenize(file.read())
+                #     if word.isalpha()
+                # ]
+                file_text = file.read()
+                print("file_text length: ", len(file_text))
+                files[filename] = file_text
+    
+    print("files count: ", len(files))
+    return files
 
 
 def tokenize(document):
@@ -59,6 +82,10 @@ def tokenize(document):
     Process document by coverting all words to lowercase, and removing any
     punctuation or English stopwords.
     """
+    # TODO: nltk has functions to remove punctuation and stop words
+    # TODO: tokenize will help solve the third example from the spec
+    # TODO: definitely need to use .tolower
+    
     raise NotImplementedError
 
 
@@ -70,6 +97,9 @@ def compute_idfs(documents):
     Any word that appears in at least one of the documents should be in the
     resulting dictionary.
     """
+    # TODO: look at the lecture notes for how to calculate the idf function
+    # TODO: brian provides the code for this
+    # TODO: make sure to use base 'e lg for this
     raise NotImplementedError
 
 
@@ -80,6 +110,7 @@ def top_files(query, files, idfs, n):
     to their IDF values), return a list of the filenames of the the `n` top
     files that match the query, ranked according to tf-idf.
     """
+    # TODO: use python sort() which lets you pass words in.. this will make it more Pythonic .. else it will just be normal code
     raise NotImplementedError
 
 
@@ -91,6 +122,7 @@ def top_sentences(query, sentences, idfs, n):
     the query, ranked according to idf. If there are ties, preference should
     be given to sentences that have a higher query term density.
     """
+    # TODO: use .sorted() with helper function of keys??
     raise NotImplementedError
 
 
